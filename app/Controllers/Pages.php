c41240eacb35653166f6ca2400d2e6abdfb9aebe
Riyan042 <riyandarmawan042@gmail.com>
2023-10-22 10:49:12 +0700

    pertemuan 2

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Home | Riyan Darmawan',
+            'tes' => ['satu', 'dua', 'tiga']
+        ];
+
+        echo view('layout/header', $data);
+        echo view('pages/home');
+        echo view('layout/footer');
+    }
+
+    public function about()
+    {
+        $data = [
+            'title' => 'About Me'
+        ];
+
+        echo view('layout/header', $data);
+        return view('pages/about');
+        echo view('layout/footer');
+    }
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus obcaecati, sed doloribus vitae qui iste dolore officia consequatur, dolorem aliquid asperiores perferendis ab similique voluptates sit, debitis enim necessitatibus quo.</p>
+        </div>
+    </div>
+</div>
