c4b1cdb6095a30976ea81781d4e530ccc47898fa
Riyan042 <riyandarmawan042@gmail.com>
2023-10-23 09:32:04 +0700

    pertemuan 8

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Pages::index');
+
+$routes->get('/komik/(:segment)', 'Komik::detail/$1');
+
 $routes->setAutoRoute(true);
---
+++ app/Controllers/Komik.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+
+    public function __construct()
+    {
+        $this->komikModel = new KomikModel();
+    }
+
+    public function index()
+    {
+        // $komik = $this->komikModel->findAll();
+
+        $data = [
+            'title' => 'Daftar Komik',
+            'komik' => $this->komikModel->getKomik()
+        ];
+
+        // cara konek db tanpa model
+        // $db = \config\Database::connect();
+        // $komik = $db->query('SELECT * FROM komik');
+        // foreach ($komik->getResultArray() as $row) {
+        //     d($row);
+        // }
+
+        // $omikModel = new \App\Models\KomikModel();
+        // $komikModel = new KomikModel();
+
+        return view('komik/index', $data);
+    }
+
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+
+        return view('komik/detail', $data);
+    }
+}
